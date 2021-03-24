c92efdedbb818dd556eae2d78aed06d0d4312924
lemode <lemode@gadventures.com>
2021-03-24 04:27:09 -0400

    load to google drive functions

--- code_playground/streamlit_src/google_drive.py
+++ code_playground/streamlit_src/google_drive.py
@@ -1,10 +1,11 @@
+import pandas as pd
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-
 def google_drive_authenticate_and_upload(
-    dataframe, target_filename, parent_folder_id, team_drive_id=None
+    dataframe, target_filename, parent_folder_id, shared_drive_id=None
 ):
+    # Authenticate and create the PyDrive client.
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
     drive = GoogleDrive(gauth)
@@ -14,14 +15,15 @@ def google_drive_authenticate_and_upload(
     #   team_drive_id = 'XXXXX'
     #   parent_folder_id = 'XXXXX-YYYYY'
 
-    if team_drive_id:
+    if shared_drive_id:
         gfile = drive.CreateFile(
             {
                 "title": target_filename,
+                "mimeType": "application/vnd.google-apps.folder",
                 "parents": [
                     {
-                        "kind": "drive#fileLink",
-                        "teamDriveId": team_drive_id,
+                        "kind": "drive#file",
+                        "driveId": shared_drive_id,
                         "id": parent_folder_id,
                     }
                 ],
@@ -35,42 +37,57 @@ def google_drive_authenticate_and_upload(
             }
         )
 
-    gfile.SetContentFile(target_filename)
+    # gfile.SetContentFile(target_filename) --only if its a file
 
     gfile.Upload(param={"supportsTeamDrives": True})
 
-    def google_drive_list_files(
-        parent_folder_id, team_drive_id=None, include_trash=False
-    ):
-        return drive.ListFile(
-            {
-                "corpora": "drive",
-                "driveId": team_drive_id,
-                "includeItemsFromAllDrives": "true",
-                "orderBy": "title",
-                "q": "parents={} and trashed={}".format(
-                    parent_folder_id, include_trash
-                ),
-                "supportsAllDrives": "true",
-            }
-        ).GetList()
 
-    def google_drive_delete_file(file_name):
-        file_list = drive.ListFile(
-            {
-                "corpora": "drive",
-                "driveId": "0AFwkOXlitG48Uk9PVA",
-                "includeItemsFromAllDrives": "true",
-                "orderBy": "title",
-                "q": "parents='1nB0B0fKxklHinZl-vIx6ddBOAo1WdAo7' and trashed=false",
-                "supportsAllDrives": "true",
-            }
-        ).GetList()
+def google_drive_list_files(parent_folder_id, shared_drive_id=None, include_trash=False):
+    # Authenticate and create the PyDrive client.
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
 
-        try:
-            for file in file_list:
-                print(file)
-                if file["title"] == "Copy of daily_fx_rates1":
-                    file.Delete()
-        except:
-            pass
+    return drive.ListFile(
+        {
+            "corpora": "drive",
+            "driveId": shared_drive_id,
+            "includeItemsFromAllDrives": "true",
+            "orderBy": "title",
+            "q": "parents={} and trashed={}".format(parent_folder_id, include_trash),
+            "supportsTeamDrives": "true",
+        }
+    ).GetList()
+
+
+def google_drive_delete_file(file_name):
+    file_list = drive.ListFile(
+        {
+            "corpora": "drive",
+            "driveId": "0AFwkOXlitG48Uk9PVA",
+            "includeItemsFromAllDrives": "true",
+            "orderBy": "title",
+            "q": "parents='1nB0B0fKxklHinZl-vIx6ddBOAo1WdAo7' and trashed=false",
+            "supportsAllDrives": "true",
+        }
+    ).GetList()
+
+    try:
+        for file in file_list:
+            print(file)
+            if file["title"] == "Copy of daily_fx_rates1":
+                file.Delete()
+    except:
+        pass
+
+
+# google_drive_authenticate_and_upload(
+#     pd.DataFrame(),
+#     "linda",
+#     "1-M86F6y5hm4J9zQNgIBdbOKND3dgKZ7V",
+#     "0AJtUdmSRqmfpUk9PVA",
+# )
+
+google_drive_list_files(
+    "1-M86F6y5hm4J9zQNgIBdbOKND3dgKZ7V",
+    "0AJtUdmSRqmfpUk9PVA")
