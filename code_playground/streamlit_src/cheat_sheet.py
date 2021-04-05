7d93da37287185b4e821a64ae8c3aa14ab5a680a
lemode <linda.emode@gmail.com>
2021-04-05 16:09:34 -0400

    Update cheat_sheet.py
    add sudo password check

--- code_playground/streamlit_src/cheat_sheet.py
+++ code_playground/streamlit_src/cheat_sheet.py
@@ -79,6 +79,7 @@ def write():
         > **Install requirements** `python -m pip install -r requirements.txt`  
         > **Deactivate environment** `deactivate`  
         > **Remove virtual environment** `rm -r .venv`  
+        > **check sudo password on Mac (should be local machine password)**[`sudo -v`](https://superuser.com/questions/553932/how-to-check-if-i-have-sudo-access)
     """
     )
 
